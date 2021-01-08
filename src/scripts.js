dc9c0ae3b13434d32295afc90483227c89e6d599
jakob-98 <jakobserlier@gmail.com>
2021-01-08 19:57:31 +0100

    added smaller chart

--- src/scripts.js
+++ src/scripts.js
@@ -79,46 +79,72 @@ let initMap = function(){
   });
 }
 
-let initChart = function(){
-  var ctx = document.getElementById('chart').getContext('2d');
-  chart = new Chart(ctx, {
-    type: 'bar',
+
+let chartoptions = {
     maintainAspectRatio: false,
     responsive: false,
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [0,0,0,0,0,0],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
+    scales: {
+        xAxes: [{
+            gridLines: {
+                display:false
+            },
+            beginatzero: true
+        }],
         yAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
+            gridLines: {
+                display:false
+            }   
         }]
-      }
+    },
+    title: {
+        position: 'bottom',
+        display: true,
+        text:'CO2 emission of [TODO]',
+    },
+    legend: {
+        display: false
+    },
+    tooltips: {
+        callbacks: {
+           label: function(tooltipItem) {
+                  return tooltipItem.yLabel;
+           }
+        }
     }
-  }); 
+}
+  
+
+console.log(mousecountry)
+
+let initChart = function(){
+    var ctx = document.getElementById('chart').getContext('2d');
+    chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                data: [20, 0, 30, 0, 0, 0],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 2.5
+            }]
+        },
+        options: chartoptions
+    }); 
 }
 
 // /// Slider
