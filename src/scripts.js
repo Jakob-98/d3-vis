4b0e794d0c8657fbfac0319abe81e1a02816c667
Wouter van Oorschot <woutervanoorschot@outlook.com>
2021-01-13 17:12:44 +0100

    rounded border, removed dummy data

--- src/scripts.js
+++ src/scripts.js
@@ -158,51 +158,6 @@ let initChart = function(){
     chart = new Chart(ctx, {
         type: 'line',
         data: {
-          labels: ["January", "February", "March", "April", "May", "June", "July"],
-          datasets: [{
-              label: "Unfilled",
-              fill: false,
-              backgroundColor: window.chartColors.blue,
-              borderColor: window.chartColors.blue,
-              data: [
-                  randomScalingFactor(),
-                  randomScalingFactor(),
-                  randomScalingFactor(),
-                  randomScalingFactor(),
-                  randomScalingFactor(),
-                  randomScalingFactor(),
-                  randomScalingFactor()
-              ],
-          }, {
-              label: "Dashed",
-              fill: false,
-              backgroundColor: window.chartColors.green,
-              borderColor: window.chartColors.green,
-              borderDash: [5, 5],
-              data: [
-                  randomScalingFactor(),
-                  randomScalingFactor(),
-                  randomScalingFactor(),
-                  randomScalingFactor(),
-                  randomScalingFactor(),
-                  randomScalingFactor(),
-                  randomScalingFactor()
-              ],
-          }, {
-              label: "Filled",
-              backgroundColor: window.chartColors.red,
-              borderColor: window.chartColors.red,
-              data: [
-                  randomScalingFactor(),
-                  randomScalingFactor(),
-                  randomScalingFactor(),
-                  randomScalingFactor(),
-                  randomScalingFactor(),
-                  randomScalingFactor(),
-                  randomScalingFactor()
-              ],
-              fill: true,
-          }]
       },
         options: chartoptions
     }); 
